5e0e28ff734bc83b711d1c959511bad69cab5ef7
Borunia <borekosp@gmail.com>
2018-03-20 19:42:27 +0100

    changes2

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,11 +1,11 @@
-var ContactForm = React.createClass({
+var contactForm = React.createClass({
     propTypes: {
       contact: React.PropTypes.object.isRequired
     },
   
     render: function() {
       return (
-        <form className={'ContactForm'}>
+        <form className={'contactForm'}>
           <input type='text' placeholder= 'Imię' value={this.props.contact.firstName} />
           <input type='text' placeholder='Nazwisko' value={this.props.contact.lastName} />
           <input type='text' placeholder='Email' value={this.props.contact.email}/>
